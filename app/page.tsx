13c822b55e45070ed0b1c8fa237f9c514e6d6605
noelfoka <nomgnenoel@gmail.com>
2024-03-08 09:58:16 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import style from '@/app/ui/home.module.css'
+import styles from '@/app/ui/home.module.css'
 
 export default function Page() {
   return (
@@ -13,7 +13,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 
           <div
-            className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+            className={styles.shape}
           />
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
